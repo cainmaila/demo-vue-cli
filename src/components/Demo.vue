63c09cee42a0d53565eca47d0c0acebde378da0e
CainChu <cainmaila@gmail.com>
2017-10-24 10:44:06 +0800

    DEMO

---
+++ src/components/Demo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="p2">
+    DEMO
+    <div>{{test}}</div>
+  </div>
+</template>
+<script>
+export default {
+  data:function() {
+    return {
+      test: "Cain!"
+    }
+  },
+}
+</script>
+<style lang="less" scoped>
+@r:#00f;
+#p2{
+  height: 200px;
+  background: @r;
+}
+div{
+  color: #f00;
+}
+</style>
+
+
+
+
+
+
