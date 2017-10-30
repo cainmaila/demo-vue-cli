c9a75408796232030b8875022f4d45e0e4069610
CainChu <cainmaila@gmail.com>
2017-10-30 20:44:50 +0800

    components

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -2,24 +2,30 @@
   <div id="p2">
     DEMO
     <div>{{test}}</div>
+    <c3 :txt2="item" v-for="item in list" ></c3>
   </div>
 </template>
 <script>
+import c3 from "@/components/d2";
 export default {
-  data:function() {
+  data: function() {
     return {
-      test: "Cain!"
-    }
+      test: "Cain!",
+      list: ["11111", "2222222", "3333333"]
+    };
   },
-}
+  components: {
+    c3
+  }
+};
 </script>
 <style lang="less" scoped>
-@r:#00f;
-#p2{
+@r: #00f;
+#p2 {
   height: 200px;
   background: @r;
 }
-div{
+div {
   color: #f00;
 }
 </style>
---
+++ src/components/d2.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>{{txt2}}</h1>
+    </div>
+</template>
+<script>
+export default {
+  props: ["txt2"]
+};
+</script>
